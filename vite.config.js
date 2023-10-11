4f4837887c22d78d7723305d4dbcc863746b49a4
Dhruv Gupta <82156134+dhruv-gupta-7@users.noreply.github.com>
2023-10-11 13:49:12 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@mui/material': '@mui/material', // Add this line to alias '@mui/material'
-    },
-  },
-});
+})
